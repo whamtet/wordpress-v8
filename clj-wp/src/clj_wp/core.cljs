a054117dc7cec49e1437371314909619fa724fd2
Matthew Molloy <whamtet@gmail.com>
2020-07-21 23:34:32 +0700

    improve logging, add as wp plugin

--- clj-wp/src/clj_wp/core.cljs
+++ clj-wp/src/clj_wp/core.cljs
@@ -5,10 +5,19 @@
   (:require
     [clojure.pprint :refer [pprint]]))
 
+(defn- prn-php-abstract [print-f o]
+  (-> o pr-str print-f))
+(defn- pprint-php-abstract [print-f o]
+  (-> o pprint with-out-str print-f))
+(defn- log-abstract [s]
+  (js/PHP.foo.__call "log" #js [s]))
+
 (defn prn-php [s]
-  (js/print (pr-str s)))
+  (prn-php-abstract js/print s))
 (defn pprint-php [s]
-  (-> s pprint with-out-str js/print))
+  (pprint-php-abstract js/print s))
+(defn log [s]
+  (pprint-php-abstract log-abstract s))
 
 (defn v8->clj [m]
   (if (goog.isObject m)
@@ -22,12 +31,10 @@
   (js/PHP.foo.__call "call" #js [f (clj->js args)]))
 
 (defn ^:export tt []
-  (invoke "barber" "hello"))
+  (log "fuck you"))
 
 (defn ^:export print-globals []
-  (pprint-php
-    (v8->clj js/PHP))
-  (js/exit))
-
+  (log
+    (v8->clj js/PHP)))
 
 ;(js/exit)
---
+++ wordpress/wp-content/plugins/awesomeplugin/awesomeplugin.php
@@ -0,0 +1,67 @@
+<?php
+   /*
+   Plugin Name: wordpress-v8
+   Plugin URI: https://github.com/whamtet/wordpress-v8
+   description: >-
+  Vroom vroom
+   Version: 0.1
+   Author: whamtet
+   Author URI: https://github.com/whamtet
+   License: GPL2
+   */
+class Foo {
+  function call($callback, $param_arr) {
+    return call_user_func_array($callback, $param_arr);
+  }
+  function log($to_log) {
+    syslog(LOG_INFO, $to_log);
+  }
+}
+function set_v8() {
+  global $v8js;
+  try {
+      $isProd = FALSE;
+      $dumpSnapshot = FALSE;
+      if ($isProd)
+      {
+        $v8js = new V8Js('PHP', array(), array(), TRUE, file_get_contents('/tmp/snapshot'));
+      } else {
+        $v8js = new V8Js();
+      }
+      $v8js->foo = new Foo;
+      $v8js->globals = $GLOBALS;
+
+      // This prints 'bar'
+      // $v8->executeString('print(PHP.foo.$bar, "\n");');
+      // This prints "I'm a function!"
+      // $v8->executeString('PHP.foo.__call("bar", ["function"]);');
+
+      if(!$isProd)
+      {
+        $v8js->executeString(file_get_contents('/app/clj_wp.js'));
+      }
+      if($dumpSnapshot)
+      {
+        $snapshot = V8Js::createSnapshot(file_get_contents('/app/clj_wp.js'));
+//         var_dump($snapshot);
+        $myfile = fopen("/tmp/snapshot", "w");
+        fwrite($myfile, $snapshot);
+        fclose($myfile);
+      }
+    } catch (Exception $e) {
+      error_log($e->getMessage());
+    }
+}
+
+function v8($func) {
+  global $v8js;
+  if (!isset($v8js)) {
+    set_v8();
+  }
+  try {
+    $v8js->executeString($func);
+  } catch (Exception $e) {
+    error_log($e->getMessage());
+  }
+}
+?>
