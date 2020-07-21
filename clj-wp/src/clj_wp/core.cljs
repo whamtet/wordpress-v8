6d487a694a1e347995b0982d78cdd1ff8983af4e
Matthew Molloy <whamtet@gmail.com>
2020-07-21 23:56:17 +0700

    improve php interop slightly

--- clj-wp/src/clj_wp/core.cljs
+++ clj-wp/src/clj_wp/core.cljs
@@ -10,7 +10,7 @@
 (defn- pprint-php-abstract [print-f o]
   (-> o pprint with-out-str print-f))
 (defn- log-abstract [s]
-  (js/PHP.foo.__call "log" #js [s]))
+  (js/PHP.foo.__call "log" #js [(.trim s)]))
 
 (defn prn-php [s]
   (prn-php-abstract js/print s))
@@ -19,19 +19,32 @@
 (defn log [s]
   (pprint-php-abstract log-abstract s))
 
+(defn keyword-safe [s]
+  (keyword
+    (if (and (string? s) (.startsWith s "$"))
+      (.substring s 1)
+      s)))
 (defn v8->clj [m]
-  (if (goog.isObject m)
+  (cond
+    (goog.isArray m)
+    (for [k (object.getKeys m)]
+      (v8->clj (object.get m k)))
+    (goog.isObject m)
     (into {}
           (for [k (object.getKeys m)
                 :when (not= "v8js" k)] ;prevent circular references
-            [(keyword k) (v8->clj (object.get m k))]))
+            [(keyword-safe k) (v8->clj (object.get m k))]))
+    :else
     m))
 
 (defn invoke [f & args]
-  (js/PHP.foo.__call "call" #js [f (clj->js args)]))
+  (js/PHP.foo.__call "call" #js [f (clj->js (or args ()))]))
 
 (defn ^:export tt []
-  (log "fuck you"))
+  (-> "GET_POSTS"
+      invoke
+      v8->clj
+      log))
 
 (defn ^:export print-globals []
   (log
